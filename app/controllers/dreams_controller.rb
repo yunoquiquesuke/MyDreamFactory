c40662001421d29531872bab2a4da9a5bb956cad
yunoquiquetanaka <yunosuke-tanaka@g.ecc.u-tokyo.ac.jp>
2018-09-27 03:19:14 +0900

    show and new, and create done

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -8,12 +8,16 @@ class DreamsController < ApplicationController
   end
 
   def show
+    @dream = Dream.find(params[:id])
   end
 
   def create
+    @dream = Dream.create(dream_params)
+    redirect_to dreams_path
   end
 
   def edit
+    @dream = Dream.find(params[:id])
   end
 
   def update
@@ -22,4 +26,8 @@ class DreamsController < ApplicationController
   def destroy
   end
 
+  private
+    def dream_params
+      params.require(:dream).permit(:name, :due)
+    end
 end
