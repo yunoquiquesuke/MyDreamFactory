9931cc8af9fb0d4e3f83c0cccee7634315ecd763
yunoquiquetanaka <yunosuke-tanaka@g.ecc.u-tokyo.ac.jp>
2018-09-27 04:06:35 +0900

    routing fixed to singular

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#index'
-  resources :dreams do
-    resources :projects
+  resources :dream do
+    resources :project
   end
 
   get '/top', to: 'top#index'
