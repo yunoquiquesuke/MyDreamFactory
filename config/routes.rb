5fe10ea0011a99dc0db7686aed529752b84652a1
yunoquiquetanaka <yunosuke-tanaka@g.ecc.u-tokyo.ac.jp>
2018-09-27 02:13:42 +0900

    routing done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'top#index'
+  resources :dreams do
+    resources :projects
+  end
+
+  get '/top', to: 'top#index'
+  get '/daily', to: 'daily#index'
+
 end
