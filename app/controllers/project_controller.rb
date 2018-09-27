6882fa8c954599a8d621f8d77c738ca88282261d
yunoquiquetanaka <yunosuke-tanaka@g.ecc.u-tokyo.ac.jp>
2018-09-27 11:37:29 +0900

    project craete done

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,7 +1,13 @@
 class ProjectController < ApplicationController
 
   def create
-    
+    @dream = Dream.find(params[:dream_id])
+    @dream.projects.create(project_params)
+    redirect_to dream_path(@dream)
   end
 
+  private
+    def project_params
+      params.require(:project).permit(:level, :name, :is_done, :due)
+    end 
 end
